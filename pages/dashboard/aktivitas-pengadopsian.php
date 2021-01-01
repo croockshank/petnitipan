8accff2ac9115da763e39bc91ac618de15e13d09
Genadi Dharma <genadi.alba@gmail.com>
2021-01-01 22:34:38 +0800

    aktivitas pengadopsian query fixed

--- pages/dashboard/aktivitas-pengadopsian.php
+++ pages/dashboard/aktivitas-pengadopsian.php
@@ -2,7 +2,7 @@
     require_once "../../core/init.php";
 
     $id_shelter = get('id-shelter');
-    $query = "SELECT COALESCE(dt.jumlah, 0) AS jumlah, MONTHNAME(am.date) AS bulan FROM( SELECT COUNT(ph.id_pengadopsi_mengadopsi_hewan) AS jumlah, ph.tanggal FROM pengadopsi_mengadopsi_hewan ph INNER JOIN hewan h ON ph.id_hewan = h.id_hewan INNER JOIN shelter s ON h.id_shelter = s.id_shelter WHERE s.id_shelter = '$id_shelter' ) dt RIGHT JOIN all_months am ON MONTH(am.date) = MONTH(dt.tanggal) WHERE am.date BETWEEN CURDATE() - INTERVAL 6 MONTH AND CURDATE() ORDER BY am.date DESC";
+    $query = "SELECT COALESCE(dt.jumlah, 0) AS jumlah, MONTHNAME(am.date) AS bulan FROM( SELECT COUNT(ph.id_pengadopsi_mengadopsi_hewan) AS jumlah, MONTH(ph.tanggal) AS bulan FROM pengadopsi_mengadopsi_hewan ph INNER JOIN hewan h ON ph.id_hewan = h.id_hewan INNER JOIN shelter s ON h.id_shelter = s.id_shelter WHERE s.id_shelter = '$id_shelter' GROUP BY MONTH(ph.tanggal)) dt RIGHT JOIN all_months am ON MONTH(am.date) = dt.bulan WHERE am.date BETWEEN CURDATE() - INTERVAL 6 MONTH AND CURDATE() ORDER BY am.date DESC";
     $result = mysqli_query($conn, $query);
 
     $aktivitas = array();
